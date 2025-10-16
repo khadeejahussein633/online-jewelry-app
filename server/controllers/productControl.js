2cd21346a45f3482ee64bd04b2774b228860c907
khadeejahussein633 <khadeejahussein633@gmail.com>
2025-10-16 11:48:10 +0300

    productg

--- server/controllers/productControl.js
+++ server/controllers/productControl.js
@@ -18,3 +18,14 @@ export const createProduct = async (req, res) => {
 
     }
 }
+
+export  const getAllProducts= async (req,res)=>{
+    try {
+ const products =await Products.find()
+    res.status(200).json(products)
+        
+    } catch (error) {
+        
+    }
+   
+} 
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { createProduct } from "../controllers/productControl.js";
+import { createProduct, getAllProducts } from "../controllers/productControl.js";
 
 
 const productRouter = express.Router()
 productRouter.post("/create-product", createProduct)
-
+productRouter.get("/read-product", getAllProducts)
 export default productRouter
