13ad4859285a2a98dac4858a30f5608c85e5e6f0
munaidiris41 <munaidiris41@gmail.com>
2025-10-16 12:01:31 +0300

    update and delete

--- server/controllers/productControl.js
+++ server/controllers/productControl.js
@@ -18,3 +18,66 @@ export const createProduct = async (req, res) => {
 
     }
 }
+
+export const updatedProduct = async (req, res) => {
+  try {
+    const {id } = req.params
+    const { name, description, price, image } = req.body;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ 
+        success: false,
+        data: null,
+        message: "product not found" });
+    }
+
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    product.image = image;
+
+  
+    const updatedProduct = await product.save();
+
+    res.status(200).json({
+        success: true,
+        data: updatedProduct,
+        message: "Product updated successfully"
+    });
+
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+
+
+export const deleteProduct = async (req, res) => {
+  try {
+  
+
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ 
+        success: false,
+        data: null,
+        message: "Product not found" });
+    }
+
+
+    res.status(200).json({
+        success: true,
+        data: null,
+        message: "Product deleted successfully"
+    });
+
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createProduct } from "../controllers/productControl.js";
+import { createProduct, deleteProduct, updatedProduct } from "../controllers/productControl.js";
 
 
 const productRouter = express.Router()
 productRouter.post("/create-product", createProduct)
+productRouter.put("/update-product/:id", updatedProduct)
+productRouter.delete("/delete-product/:id", deleteProduct)
 
 export default productRouter
